5f51a7cee4fe64825611e4cb28625e1754c4a7d7
elias <datwii1234@gmail.com>
2023-05-31 21:07:08 +0300

    dc;deaqlwgkrkfw

--- Project4/Controllers/EmailController.cs
+++ Project4/Controllers/EmailController.cs
@@ -18,18 +18,18 @@ namespace Project4.Controllers
         public IActionResult SendEmail(string body, string user, string email1, string d1, string d2)
         {
             var email = new MimeMessage();
-            email.From.Add(MailboxAddress.Parse("monica.kihn25@ethereal.email"));
+            email.From.Add(MailboxAddress.Parse("rowsa2000@gmail.com"));
             email.To.Add(MailboxAddress.Parse(email1));
             email.Subject = "booking";
             email.Body = new TextPart(TextFormat.Html)
             {
                 Text = "dear " + user +
                 "have booking from " + d1 + " to " + d2 +
-                "sdwdaedjqdiqodn djkqwdnoqwidmqw;dqw qw dqd qd qdd" + body + "doqdm;qwkp;l;d"
+                "sdwdaedjqdiqodn djkqwdnoqwidmqw;dqw qw dqd qd qdd" + body + "uyhvwiucbjerndgg"
             };
             using var smtp = new SmtpClient();
-            smtp.Connect("smtp.ethereal.email", 587, SecureSocketOptions.StartTls);
-            smtp.Authenticate("monica.kihn25@ethereal.email", "pPhafpb7TEWR57e9H9");
+            smtp.Connect("smtp.gmail.com", 587, SecureSocketOptions.StartTls);
+            smtp.Authenticate("rowsa2000@gmail.com", "uyhvwiucbjerndgg");
             smtp.Send(email);
             smtp.Disconnect(true);
             return Ok();
